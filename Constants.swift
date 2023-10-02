470e141c1eef839a2bfabdd9779590ada31a29da
Marcus <1757743+mjhirst@users.noreply.github.com>
2023-10-02 23:28:39 +0100

    Create README.md

---
+++ Constants.swift
@@ -0,0 +1,30 @@
+//
+//  Constants.swift
+//  ChristinaKeyboard
+//
+//  Created by Marcus Hirst on 20/04/2016.
+//  Copyright © 2016 Marcus. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+struct Global {
+    static let Batch_Dev_Key = ""
+    static let Batch_Key = ""
+    
+    static let SwiftyBeaver_AppID = ""
+    static let SwiftyBeaver_Secret = ""
+    static let SwiftyBeaver_EncryptionKey = ""
+}
+
+struct GlobalColours {
+    static let background = UIColor(colorLiteralRed:0.106, green:0.122, blue:0.149, alpha:1.000)
+    static let reddish = UIColor(red:0.851, green:0.220, blue:0.161, alpha:1.000)
+    static let minty = UIColor(red:0.702, green:0.953, blue:0.800, alpha:1.000)
+    static let yellow = UIColor(red:0.984, green:0.812, blue:0.263, alpha:1.000)
+    static let purple = UIColor(red:0.702, green:0.314, blue:0.663, alpha:1.000)
+    static let cellBackground = UIColor(red:0.192, green:0.224, blue:0.282, alpha:1.000)
+    static let cellSeparator = UIColor(red:0.467, green:0.498, blue:0.561, alpha:1.000)
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# CristinaKeyboard
+
+This is a now-defunct birthday present for a friend. Christina Yang from Grey's Anatomy got us through some rough times!
+Surprisingly, v1.0 made it to the appstore. A subsequent update was rejected by review.
